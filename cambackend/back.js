2e2289f4c560ced81e83ab5824294a2cd1b7a3da
Shubham <shubhambade3@gmail.com>
2023-04-14 15:39:25 +0530

    Live capture and upload functionality completed

--- cambackend/back.js
+++ cambackend/back.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
--- frontend/src/NewApp.js
+++ frontend/src/NewApp.js
@@ -4,6 +4,7 @@ import Webcam from 'react-webcam';
 const NewApp = () => {
   const webcamRef = useRef(null);
   const [image, setImage] = useState(null);
+  const [base64String, setBase64String]= useState(null);
 
   const [disimg, setDisimg] = useState("");
   const [allimage, setAllimage] = useState([]);
@@ -16,17 +17,9 @@ const NewApp = () => {
 
     useEffect(()=>{
         getImage()
-    })
-  const download = () => {
-    // Check if there is a captured image to download
-    if (!image) return;
+    },[])
 
-    // Create a temporary link element to download the image
-    const link = document.createElement('a');
-    link.href = image;
-    link.download = 'captured_image.jpeg';
-    link.click();
-  };
+
 
   const convertToBase64=(event)=>{
     console.log(event);
@@ -43,20 +36,56 @@ const NewApp = () => {
     };
   }
 
-  const uploads=()=>{
-    fetch("http://localhost:4000/upload-image",{
-        method:"POST",
-        crossDomain:true,
-        headers:{
-            "Content-Type":"application/json",
-            Accept:"application/json",
-            "Access-Control-Allow-Origin":"*",
+  const uploads = async () => {
+    try {
+      const capdata = await download(); // wait for download to complete
+      console.log("value in uploads :", capdata);
+      await fetch("http://localhost:4000/upload-image", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
         },
-        body:JSON.stringify({
-            base64:disimg
+        body: JSON.stringify({
+          base64: disimg ? disimg : capdata, // use the downloaded image data
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const download = async () => {
+    return new Promise((resolve, reject) => {
+      // Check if there is a captured image to download
+      if (!image) reject("No image to download");
+  
+      // Create a temporary link element to download the image
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'captured_image.jpeg';
+      console.log(link.download);
+  
+      fetch(link.href)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(blob);
+          reader.onload = () => {
+            const capdata = reader.result;
+            console.log(capdata);
+            setBase64String(capdata);
+            resolve(capdata); // resolve the promise
+          };
         })
-    }).then((res)=>res.json()).then((data)=>console.log(data))
-  }
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
 
   const getImage=()=>{
     fetch("http://localhost:4000/send-image",{
@@ -80,20 +109,24 @@ const NewApp = () => {
         </div>
       </div>
       <div className="d-flex justify-content-center my-5" >
-        <input accept="image/*" type='file' onChange={convertToBase64}/>
+        {/* <input accept="image/*" type='file' onChange={convertToBase64}/> */}
         <button className="btn btn-primary me-3" style={{borderRadius:"8px"}} onClick={capture}>
           Capture
         </button>
-        <button className="btn btn-success" style={{borderRadius:"8px"}}  onClick={download}>
+        {/* <button className="btn btn-success" style={{borderRadius:"8px"}}  onClick={download}>
           Download
-        </button>
+        </button> */}
         <button className="btn btn-success mx-3" onClick={uploads} style={{borderRadius:"8px"}}>
           Upload
         </button><br/>
       </div>
-      <div className="d-flex justify-content-center my-3">
-        {disimg==="" || disimg===null ? "" : <img className="mx-3" src={disimg} alt="Captured" /> }
-      </div>
+      {/* <div className="d-flex justify-content-center my-3" >
+        {disimg==="" || disimg===null ? "" : <img className="mx-3" width={100}
+                height={100} src={disimg} alt="Captured" /> }
+                
+        {base64String==="" || base64String===null ? "" : <img width={100}
+                height={100} className="mx-3" src={base64String} alt="Captured" /> }
+      </div> */}
       <div className="d-flex justify-content-center my-5">
       {allimage.map(data=>{
             return(<img
